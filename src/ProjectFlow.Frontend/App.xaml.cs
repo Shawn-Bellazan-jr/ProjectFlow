82ceb3378179933d4801e665d4f9a1d64b1332e8
Shawn-Bellazan-jr <166971628+Shawn-Bellazan-jr@users.noreply.github.com>
2024-05-19 09:01:33 -0500

    feat(frontend): Create Blazor MAUI project
    Create a new Blazor MAUI project using the .NET CLI.

---
+++ src/ProjectFlow.Frontend/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectFlow.Frontend
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/ProjectFlow.Frontend/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectFlow.Frontend
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
